809af03cbc353a5ed20fa706af7993b877182b76
João Biasoli <joao.biasoli@edu.pucrs.br>
2026-05-18 18:29:42 -0300

    feat: UC6 - adiciona endpoint DELETE /pedidos/{id} e CancelarPedidoUC

---
+++ src/main/java/com/bcopstein/ex4_lancheriaddd_v1/Aplicacao/CancelarPedidoUC.java
@@ -0,0 +1,20 @@
+package com.bcopstein.ex4_lancheriaddd_v1.Aplicacao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.bcopstein.ex4_lancheriaddd_v1.Dominio.Servicos.IPedidoService;
+
+@Component
+public class CancelarPedidoUC {
+    private IPedidoService pedidoService;
+
+    @Autowired
+    public CancelarPedidoUC(IPedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
+
+    public void executar(long id, String cpf) {
+        pedidoService.cancelar(id, cpf);
+    }
+}
